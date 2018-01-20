0fe94d144efc0cbbca09bdd3be4622a5e0959fd2
SeeratSek <seeratsek@gmail.com>
2018-01-20 15:10:36 -0800

    add navigation and state for last page

--- client/App.js
+++ client/App.js
@@ -5,34 +5,46 @@ import { Router, Route, hashHistory } from 'react-router';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmitHome = this.handleSubmitHome.bind(this);
+    this.handleSubmitForm = this.handleSubmitForm.bind(this);
 
     this.state = {
-    	submitted: false,
+    	page: 'home',
     	labels: [],
+    	tweeter: '',
     }
   }
 
-  handleSubmit(e) {
+  handleSubmitHome(e) {
+  	console.log(e);
   	e.preventDefault();
   	// send request for tweets and store result in this.state.labels
   	// mock data 
-  	this.state.labels = ['noun', 'test2'];
-  	this.setState({'submitted': true });
+  	this.state.labels = ['noun', 'adjective', 'noun', 'verb'];
+  	this.setState({'page': 'myform' });
+  }
+
+  handleSubmitForm(e) {
+  	e.preventDefault();
+
+  	this.setState({ 'page': 'home' });
   }
 
   render() {
-  	if (this.state.submitted) {
-  		return (<MyForm labels= { this.state.labels }> </MyForm>)
+  	if (this.state.page == 'myform') {
+  		return (<MyForm onSubmit= { this.handleSubmitForm } labels= { this.state.labels }> </MyForm>)
   	}
-  	else {
+  	else if (this.state.page == 'tweetResult') {
+  		return;
+  	}
+  	else if (this.state.page == 'home') {
   		return (
-      	<form onSubmit={ this.handleSubmit }>	
+      	<form onSubmit={ this.handleSubmitHome }>	
   				<label>Choose tweeter</label>
   				<input type="text" />
-        <button>Submit</button>
-      </form>
-    );
+        	<button>Submit</button>
+      	</form>
+    	);
   	}
     
   }
