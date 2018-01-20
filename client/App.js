765fb09cc9a7f9aa49015118507edcb8c1d68119
SeeratSek <seeratsek@gmail.com>
2018-01-20 14:02:19 -0800

    add input form

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MyForm from './MyForm';
 
 class App extends Component {
     constructor(props) {
@@ -7,7 +8,7 @@ class App extends Component {
 
     render() {
         return (
-            <div>Hello World!</div>
+            <MyForm></MyForm>
         );
     }
 }
